ab17c35cc494b712e6c2e77c706480725497fbe1
franky4p_mac <franky4p@yandex.ru>
2021-11-10 13:32:13 +0300

    create app

---
+++ MapsSwiftUI/MapsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapsSwiftUI
+//
+//  Created by Pavel Khlebnikov on 10.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapsSwiftUI/MapsSwiftUI/MapsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsSwiftUIApp.swift
+//  MapsSwiftUI
+//
+//  Created by Pavel Khlebnikov on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
